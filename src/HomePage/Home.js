1b195b45425885f29e954fdec294833db18a7870
imrahulv <181220046@nitdelhi.ac.in>
2021-04-17 20:02:34 +0530

    changed homepage

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -7,9 +7,11 @@ class Home extends Component {
     return (
       <div className="Content">
         <div className="TextBox">
-          <h2>The Sparks Foundation <br/><span>Basic Banking System</span></h2>
-          <p>It is a simple dynamic website in which one can see all the customers and their details and their current amount. We can also send amount from one customer to another customer and can also see the transaction history.</p>
-          <h3>by-<span>Rahul Kumar Verma</span></h3>
+          <h2>Welcome To <br/><span>TSF BANK</span></h2>
+          <h5><span>Basic Banking System</span></h5>
+          <p>It is a simple dynamic website in which one can see all the customers and their details and their current amount. We can also send amount from one customer to another customer and can also see their transaction history.</p>
+          <p>©2021 Made by <strong>Rahul Kumar Verma</strong></p>
+          
         </div>
         <div className="Imgbox">
             <img src={moneyHome} alt="money" className="Money"/>
